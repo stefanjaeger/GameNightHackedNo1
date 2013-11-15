3313fe538c8a0ea6f4d1a5e5d8086ca017635e7f
Nicolas Oeschger (nioe) <nioe@zuehlke.com>
2013-11-15 18:35:02 +0000

    Basic message handling

--- ballerburg-websockets/src/main/webapp/scripts/main.js
+++ ballerburg-websockets/src/main/webapp/scripts/main.js
@@ -1,6 +1,7 @@
 
 var ui = new UI();
 var scene = new Scene();
+var ws;
 
 /* Main entrypoint of ballerburg.js */
 function main() {
@@ -9,20 +10,23 @@ function main() {
 	scene.draw();
 	
 	ui.registerListeners();
-	ui.setGameId("ASDF");
 	
 	connectToServer();
 }
 
 function createGame() {
-	document.getElementById('welcomeScreen').className = 'hidden';
-	document.getElementById('createScreen').className = 'full-overlay';
+    ui.showCreateGame();
+    sendCreateGame();
 };
 
 function joinGame() {
-	document.getElementById('welcomeScreen').className = 'hidden';
-	document.getElementById('joinScreen').className = 'full-overlay';
+    ui.showJoinGame();
 };
+
+function joinGameWithGameId() {
+    var gameId = document.getElementById('otherGameId').value;
+    sendJoinGame(gameId);
+}
 
 function connectToServer() {
 	var target = "ws://"+window.location.host+window.location.pathname+"websocket/echo";
@@ -38,7 +42,21 @@ function connectToServer() {
 		console.log('Info: WebSocket connection opened.');
 	};
 	ws.onmessage = function (event) {
-		console.log('Received: ' + event.data);
+		var msg = event.data.split(" ");
+        switch(msg[0])
+        {
+            case 'token':
+                var gameId = msg[1];
+                console.log('Created game with ID ' + gameId);
+                ui.setGameId(gameId);
+                break;
+            case 'joined':
+                console.log('Joinded Game with ID ' + msg[1]);
+                ui.showControls();
+                break;
+            default:
+                alert('Unknown command ' + msg[0]);
+        }
 	};
 	ws.onclose = function () {
 	  	console.log('Info: WebSocket connection closed.');
@@ -46,5 +64,12 @@ function connectToServer() {
 	window.onclose = function() { ws.close(); };
 };
 
+function sendCreateGame() {
+    ws.send("create_game");
+}
+
+function sendJoinGame(gameId) {
+    ws.send("join_game " + gameId);
+}
 
 
